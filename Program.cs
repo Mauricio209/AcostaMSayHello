d5665fc731fc2f139ad42e1c11c37f9f00c60687
Mauricio Acosta <bernardoz209@icloud.com>
2022-10-19 10:33:56 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Mauricio Acosta
+//10-18-22
+// Git Challange Say Hello
+Console.Clear();
+string replay = "yes";
+while (replay == "yes")
+{
+    Console.WriteLine("What is your name?:");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + myName);
+    Console.WriteLine("Type yes to replay");
+replay = Console.ReadLine();
+}
